36b39cd49dc594e224612469e212b1225da39e89
Ivan-mwaura <94759966+Ivan-mwaura@users.noreply.github.com>
2022-10-18 17:19:31 +0300

    first commit

---
+++ caption.js
@@ -0,0 +1,11 @@
+function show(){
+    let slide = document.getsElementById('caption');
+
+        if(slide.display === "none"){
+            slide.style.display = "block";
+        }
+        else{
+            slide.style.display = "none";
+        }
+
+}
